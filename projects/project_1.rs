639312513e0a8787560757a0af245f2b2e4f403e
vosugba <vincent.osugbapau.edu.ng>
2026-08-31 13:29:01 +0100

    Project_1 commit.1

--- projects/project_1.rs
+++ projects/project_1.rs
@@ -4,12 +4,14 @@ fn main (){
 	let n:f64 = 5.00;
 
 	//Amount
-	let a:f64 = p*(1.00 + (r/100.00).powf(n.into()));
+	let a:f64 = p*(1.00 + (r/100.00).powf(n));
 	println! ("Amount equals to {}",a);
 
 	//Compound interest 
 	let ci:f64 = a - p;
 	println!("Compound Interest equals to {}",ci);
+
+	println! ("Thus for a principal of {} and with a rate of {} for {}years the Compound Interest is {}",p,r,n,ci);
 
 
 }
