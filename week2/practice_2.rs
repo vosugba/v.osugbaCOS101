7e5d2ecff047ca79a493956f00f0f45787595d70
vosugba <vincent.osugbapau.edu.ng>
2026-08-31 11:39:22 +0100

    Correcting mistake

--- week2/practice_2.rs
+++ week2/practice_2.rs
@@ -1,6 +1,6 @@
 fn main (){
 	println! (); //print just a
 	println! ("hello"); // prints "hello"
-	println! ("format {} arguments", "some");
+	println! ("format {} arguments", "some"); // print "format some arguments"
 	println! ("My name is {}. I not the class rep and do not want that stress", "Vincent Osugba");
 }
