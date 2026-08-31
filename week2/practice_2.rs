c252021640ff08932d6d43a21aae4ec18315ac3b
vosugba <vincent.osugbapau.edu.ng>
2026-08-31 11:37:01 +0100

    Practice_2 commit

---
+++ week2/practice_2.rs
@@ -0,0 +1,6 @@
+fn main (){
+	println! (); //print just a
+	println! ("hello"); // prints "hello"
+	println! ("format {} arguments", "some");
+	println! ("My name is {}. I not the class rep and do not want that stress", "Vincent Osugba");
+}
