7c6649dea98f8c425810d168ac4dd9286cf74bd3
vosugba <vincent.osugbapau.edu.ng>
2026-09-07 16:36:23 +0100

    practice2 commit cargo

---
+++ week3/practice_2/target/debug/practice_2.d
@@ -0,0 +1,1 @@
+C:\Users\vince\OneDrive\Documents\CS\ docs\COS101\ practical\v.osugbaCOS101\week3\practice_2\target\debug\practice_2.exe: C:\Users\vince\OneDrive\Documents\CS\ docs\COS101\ practical\v.osugbaCOS101\week3\practice_2\src\main.rs
