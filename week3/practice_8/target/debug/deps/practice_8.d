87d5c92b15c71368d237c2bed2736fc4fd1bccea
vosugba <vincent.osugbapau.edu.ng>
2026-09-07 17:51:25 +0100

    project_8 commit cargo

---
+++ week3/practice_8/target/debug/deps/practice_8.d
@@ -0,0 +1,5 @@
+C:\Users\vince\OneDrive\Documents\CS docs\COS101 practical\v.osugbaCOS101\week3\practice_8\target\debug\deps\practice_8.d: src\main.rs
+
+C:\Users\vince\OneDrive\Documents\CS docs\COS101 practical\v.osugbaCOS101\week3\practice_8\target\debug\deps\practice_8.exe: src\main.rs
+
+src\main.rs:
---
+++ week3/practice_8/target/debug/practice_8.d
@@ -0,0 +1,1 @@
+C:\Users\vince\OneDrive\Documents\CS\ docs\COS101\ practical\v.osugbaCOS101\week3\practice_8\target\debug\practice_8.exe: C:\Users\vince\OneDrive\Documents\CS\ docs\COS101\ practical\v.osugbaCOS101\week3\practice_8\src\main.rs
